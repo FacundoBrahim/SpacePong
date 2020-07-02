4bd1a4577ee3f21c04dfaf97950cca057523aa5a
FacundoBrahim <facundo.e.brahim@gmail.com>
2020-07-02 03:40:51 -0300

    Cambio de version

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Game : MonoBehaviour
-{
-    public Vector2 sizes = new Vector2(8, 5);
+public class Game : MonoBehaviour {
+    public Vector2 sizes = new Vector2 (8, 5);
     public types type;
-    public enum types
-    {
+    public enum types {
         INTRO,
         PLAYING,
         GAMEOVER
@@ -20,26 +18,20 @@ public class Game : MonoBehaviour
     public Player.types playerActiveType;
     public ParticleManager particleManager;
 
-    public void Win(int playerID, int score)
-    {
-        if (playerID == 1)
-        {
+    public void Win (int playerID, int score) {
+        if (playerID == 1) {
             score_player_1 += score;
-            field_score_p1.text = score_player_1.ToString();
-        }
-        else
-        {
+            field_score_p1.text = score_player_1.ToString ();
+        } else {
             score_player_2 += score;
-            field_score_p2.text = score_player_2.ToString();
+            field_score_p2.text = score_player_2.ToString ();
         }
 
-        if (score_player_1 >= 150)
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver2");
+        if (score_player_1 >= 150) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene ("GameOver2");
         }
-        if (score_player_2 >= 150)
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+        if (score_player_2 >= 150) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene ("GameOver");
         }
     }
 }
